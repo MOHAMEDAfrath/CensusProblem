5fd3c62ad2159f0524b4f4ebcc508e8f3d42f6fa
MOHAMEDAfrath <afrathismath@gmail.com>
2021-07-31 21:19:10 +0530

    [Afrath]Welcome to Census Problem

---
+++ CensusProblem/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace CensusProblem
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
